ead02f255a3ade711c71a0216f15b98fdcba1a4d
HannaPleshko <hannapleshko2000@mail.ru>
2023-08-23 10:20:18 +0300

    adaptive refactoring

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,19 @@
-import style from './style.module.css'
+import style from './style.module.css';
 
 function Header() {
-    return (
-        <div className={style.wrapper}>
-            <div className={style.item}>
-                <div className={style.logoImg}></div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.item}>
+        <div className={style.logoImg}></div>
 
-                <div>
-                    <p>Target audience</p>
-                    <p>Summary</p>
-                    <p>Speaker</p>
-                </div>
-            </div>
+        <div>
+          <p>Target audience</p>
+          <p>Summary</p>
+          <p>Speaker</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/Pages/PreviewPage/PreviewPage.jsx
+++ src/Pages/PreviewPage/PreviewPage.jsx
@@ -1,22 +1,22 @@
-import DirectorStrategy from "../../Components/DirectorStrategy/DirectorStrategy";
-import Header from "../../Components/Header/Header";
-import KeyBenefits from "../../Components/KeyBenefits/KeyBenefits";
-import Overview from "../../Components/Overview/Overview";
+import DirectorStrategy from '../../Components/DirectorStrategy/DirectorStrategy';
+import Header from '../../Components/Header/Header';
+import KeyBenefits from '../../Components/KeyBenefits/KeyBenefits';
+import Overview from '../../Components/Overview/Overview';
 
-import style from './style.module.css'
+import style from './style.module.css';
 
 function PreviewPage() {
-    return (
-        <>
-            <div className={style.previewBcg}>
-                <Header />
-                <Overview />
-            </div>
+  return (
+    <>
+      <div className={style.previewBcg}>
+        <Header />
+        <Overview />
+      </div>
 
-            <KeyBenefits />
-            <DirectorStrategy />
-        </>
-    );
+      <KeyBenefits />
+      <DirectorStrategy />
+    </>
+  );
 }
 
 export default PreviewPage;
